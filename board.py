16693fe960fa9f54670cae1c36fc01737158dfd7
Danny Levinstein <dan.levinstein@gmail.com>
2021-06-25 17:08:47 -0700

    filling in board.py methods

--- board.py
+++ board.py
@@ -10,27 +10,41 @@ class board():
         Group 2: [R10, B10, K10]
         ...
         '''
+        print('BOARD')
         print(*['Group {}: [{}]'.format(i+1,', '.join(boardGroup)) for i, boardGroup in enumerate(self.board)], sep='\n')
 
-    def add(self,group):
+    def addGroups(self,groups):
         '''
         add a group to the board
         '''
-        self.board.append(group)
+        self.board.extend(groups)
 
-    def remove(self, groupIndex):
+
+    def removeGroups(self, groups):
         '''
         remove a group from the board based on index
         '''
-        return self.board.pop(groupIndex)
+        for group in groups:
+            self.board.remove(group)
 
     def makeSelection(self, selectionIndices):
         '''
         take in indices, return groups from the board with those indices
         '''
-        self.selection = [self.board[int(i)] for i in selectionIndices.split(' ')] 
+        self.selection = [self.board[int(i)] for i in selectionIndices.split(' ')]
+        self.removeGroups(self.selection)
 
     def displaySelection(self):
         '''
         print out selected groups from makeSelection()
         '''
+        print('SELECTED GROUPS')
+        print(*['[{}]'.format(', '.join(selectedGroup)) for selectedGroup in self.selection], sep='\n')
+    
+    def reinsertSelection(self):
+        '''
+        if the group(s) submitted by the player are invalid, return the selection to the board
+        empty the selection
+        '''
+        self.board.extend(self.selection)
+        self.selection = []
