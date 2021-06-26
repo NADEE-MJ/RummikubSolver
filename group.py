8dd4e48bf0e4fc63284ca1ed86f146b51d5d95e8
NADEE-MJ <nadeem.maida@gmail.com>
2021-06-25 18:04:05 -0700

    fixed group class logic errors

--- group.py
+++ group.py
@@ -56,22 +56,24 @@ class group():
                     break
 
             #set max length check
-            if len(self.group) <= 4 and setCheck:
-                #unique colors check
-                usedColors = []
-                for tile in self.group:
-                    if tile.color in usedColors:
-                        raise UniqueColorError
-                    else:
-                        usedColors.append(tile.color)
-            else:
-                raise SetError
+            if setCheck:
+                if len(self.group) <= 4:
+                    #unique colors check
+                    usedColors = []
+                    for tile in self.group:
+                        if tile.color in usedColors:
+                            raise UniqueColorError
+                        else:
+                            usedColors.append(tile.color)
+                else:
+                    raise SetError
 
             #run check (same color)
             #get starting color
             for tile in self.group:
                 if tile.color != 'J':
                     tempColor = tile.color
+                    break
 
             #check all colors are the same or a joker
             for tile in self.group:
@@ -84,14 +86,15 @@ class group():
             jokerCount = 0
             for tile in self.group:
                 if tile.value != 0:
-                    tempNum = tile.value - jokerCount
+                    tempNum = tile.value - jokerCount - 1
+                    break
                 else:
                     jokerCount += 1
 
             #checks each tile in a run is sequential
             numbersInGroup = []
             for tile in self.group:
-                if tile.value > tempNum or tile.value == 0:
+                if tile.value == tempNum + 1 or tile.value == 0:
                     if tile.value > 0:
                         tempNum = tile.value
                         numbersInGroup.append(tempNum)
@@ -101,6 +104,14 @@ class group():
                 else:
                     raise RunError
             
+            #Tenshis way
+            for i in range(1, len(self.group)):
+                if self.group[i] > self.group[i - 1] or self.group[i] == 0:
+                    if self.group[i] != 0:
+                        numbersInGroup.append(self.group[i])
+                    else:
+                        numbersInGroup.append(0)
+
             #count the number of jokers in the run
             jokerCount = 0
             for tile in self.group:
@@ -117,9 +128,12 @@ class group():
             elif jokerCount == 2:
                 if 1 in numbersInGroup and self.group[0] != 1:
                     raise InvalidJokerError
-                if 2 in numbersInGroup and self.group[1] != 2:
+                if 2 in numbersInGroup and self.group[2] == 2:
                     raise InvalidJokerError
-                if 12 in numbersInGroup and self.group[-2] != 12:
+                if 12 in numbersInGroup and self.group[-3] == 12:
                     raise InvalidJokerError
                 if 13 in numbersInGroup and self.group[-1] != 13:
                     raise InvalidJokerError
+
+        else:
+            raise GroupError
